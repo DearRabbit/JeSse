bac07ad825f38bf70657a45260aa012a5a4c47e5
DearRabbit <DearRabbit@users.noreply.github.com>
2015-12-24 11:28:41 +0800

    add typeobj-func

--- object.h
+++ object.h
@@ -32,13 +32,23 @@ typedef struct {
 #define Js_GetSize(obj)		(((JsObject*)(obj)) -> ob_size)
 
 // typedef - function ptr groups:
-// 
-typedef JsObject * (*unaryfunc)(JsObject *);
-typedef JsObject * (*binaryfunc)(JsObject *, JsObject *);
-typedef JsObject * (*ternaryfunc)(JsObject *, JsObject *, JsObject *);
+// operators
+typedef JsObject* (*unaryfunc)(JsObject *);
+typedef JsObject* (*binaryfunc)(JsObject *, JsObject *);
+typedef JsObject* (*ternaryfunc)(JsObject *, JsObject *, JsObject *);
 typedef int (*inquiry)(JsObject *);
 typedef size_t (*lenfunc)(JsObject *);
-
+// methods
+// newfunc: TypeObject *type, Object *args, Object *kwds
+// args stores arguments without key, kwds is a list
+typedef JsObject *(*newfunc)(JsTypeObject *, JsObject *, JsObject *);
+typedef void (*destructor)(JsObject *);
+// print to FILE* fp
+typedef int (*printfunc)(JsObject *, FILE *);
+typedef JsObject* (*tostringfunc)(JsObject *);
+// return -1 if i<j, 1 if i>j, 0 if equals
+typedef int (*cmpfunc)(JsObject *, JsObject *);
+typedef long (*hashfunc)(JsObject *); 
 
 // Object - define:
 // TypeObject
@@ -47,6 +57,20 @@ typedef struct _typeobject {
 	const char *tp_name; /* For printing */
 	size_t tp_basicsize, tp_itemsize; /* For allocation */
 
+	/* Flags to define presence of optional/expanded features */
+    long tp_flags;
+
+	/* Methods to implement standard operations */
+	newfunc tp_new;
+	destructor tp_dealloc;
+	printfunc tp_print;
+	cmpfunc tp_compare;
+	tostringfunc tp_str;
+	hashfunc tp_hash;
+	
+	/* user-defined class def*/
+	//struct JsMethodDef *tp_methods;
+	//struct JSMemberDef *tp_members;
 } JsTypeObject;
 
 
