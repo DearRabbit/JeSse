7417bc957e11548fbe926add275ebd38fa070b38
DearRabbit <DearRabbit@users.noreply.github.com>
2015-12-30 09:26:40 +0800

    move logger into utility

--- utility.h
+++ utility.h
@@ -2,6 +2,16 @@
 
 #define JS_DEBUG
 
-#include "debugLogger.h"
+#ifdef JS_DEBUG
+#define dbgprint(format,args...) \
+	fprintf(stderr, format, ##args)
+#else
+	#define dbgprint(format,args...)
+#endif
+
+#define FATAL_ERROR(format,args...) \
+	fprintf(stderr, format, ##args),\
+	exit(-1)
+
 
 #define CHAR_BIT 8
