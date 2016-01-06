c759b3d36f5a1cc8d7d00c67fa3f70f232e62e83
DearRabbit <DearRabbit@users.noreply.github.com>
2016-01-06 18:37:06 +0800

    rewrite num.h

---
+++ numobject.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "object.h"
+
+typedef struct {
+	JsObject_HEAD
+	int int_flag; //even more than 53bits
+	double ob_ival;
+} JsNumObject;
+
+extern JsTypeObject JsNum_Type;
+extern int errnoInNum;
+
+#define JsNum_CheckType(obj) ((obj)->ob_type == &JsNum_Type)
+
+#define JsNum_SetInt(obj) (((JsNumObject *)(obj))->int_flag = 1)
+#define JsNum_SetFloat(obj) (((JsNumObject *)(obj))->int_flag = 0)
+#define JsNum_GetIntFlag(obj) (((JsNumObject *)(obj))->int_flag)
+
+// C - API
+JsObject* JsNum_FromDouble(double dval, int int_flag);
+double JsNum_GetDouble(JsObject *);
+
+// macro access for speed?
+#define JsNum_AS_DOUBLE(obj) (((JsNumObject *)(obj))->ob_ival)
